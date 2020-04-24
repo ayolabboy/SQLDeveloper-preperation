faf9a2ce5fc025fbc9e53dd61edb9ecee8d98810
Labboy <ayolabboy@gmail.com>
2020-04-24 13:54:46 +0900

    2020-04-24 group by making

--- GROUP BY 정리.sql
+++ GROUP BY 정리.sql
@@ -1,1 +1,13 @@
 -- group by Æ¯Áý 
+
+
+select * from testy; 
+
+
+select t_age,count(*),sum(t_age),avg(t_age),max(t_age),min(t_age),stddev(t_age) from testy
+    group by t_age;
+    
+    
+select t_age,count(*),sum(t_age) from testy
+    group by t_age
+    having sum(t_age) >0;
